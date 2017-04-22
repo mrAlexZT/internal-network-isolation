b422069668c077cbe92d6f133513ac140427ba9e
mrAlexZT <mr.AlexZT@gmail.com>
2017-04-22 09:36:14 +0300

    fix

---
+++ scripts/removeIPfomwhitelist.js
@@ -0,0 +1,30 @@
+var oEnvInfo,
+	oNodes,
+	i, n,
+	aResult = [],
+	AllNodes = "";
+
+oEnvInfo = jelastic.env.control.GetEnvInfo("${env.envName}", session);
+
+if (!oEnvInfo || oEnvInfo.result != 0) {
+	return oEnvInfo;
+}
+
+oNodes = oEnvInfo.nodes;
+
+//aResult = {result: 0, onAfterReturn: []};
+
+for (i = 0, n = oNodes.length; i < n; i += 1) {
+	//AllNodes += oNodes[i].id + ";";
+
+	aResult.push({
+		removeIPfomwhitelistProcess: {
+			AllNodes: oNodes[i].id
+		}
+	});
+}
+
+return {
+	result: 0,
+	onAfterReturn: aResult
+}
